4a30349570c33546d71b98ebca521c78060e4d7f
marcadrian-it <marcadrian.it@gmail.com>
2023-05-22 20:57:10 +0200

    half layout

---
+++ inject_brand/main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.navbar ul');
+  
+    hamburger.addEventListener('click', () => {
+      navLinks.classList.toggle('active');
+    });
+  });
+  
